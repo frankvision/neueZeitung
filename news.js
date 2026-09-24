7681e5e80cd32af4f8516c01b5f8ada4459d7097
frankvision <132785044+frankvision@users.noreply.github.com>
2026-09-24 19:56:19 +0200

    Create news.js

---
+++ news.js
@@ -0,0 +1,36 @@
+const { createClient } = supabase;
+
+const db = createClient(
+  "https://mfxslywkifmdnptkzsui.supabase.co",
+  "sb_publishable_vLkGBUUlU3Haq6_w9pcOnQ_X5PXoMQe"
+);
+
+async function loadArticles() {
+  const { data: articles, error } = await db
+    .from("articles")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.error("Supabase Fehler:", error);
+    return;
+  }
+
+  console.log(articles);
+
+  const container = document.querySelector(".articles");
+
+  articles.forEach(article => {
+    const element = document.createElement("article");
+
+    element.innerHTML = `
+      <h2>${article.title}</h2>
+      <p>${article.excerpt ?? ""}</p>
+      <p>${article.category ?? ""}</p>
+    `;
+
+    container.append(element);
+  });
+}
+
+loadArticles();
